a7d6267564db9e7807baccaabf51ed65a137a473
Tom <tom@supercast.com>
2023-07-18 07:58:16 -0400

    Define enum as map

--- app/models/audits1984/audit.rb
+++ app/models/audits1984/audit.rb
@@ -3,7 +3,11 @@ module Audits1984
     belongs_to :session, class_name: "Console1984::Session", touch: true
     belongs_to :auditor, class_name: Audits1984.auditor_class
 
-    enum status: %i[ pending approved flagged ], _prefix: "audit"
+    enum status: {
+      pending: 0,
+      approved: 1, 
+      flagged: 2
+    }, _prefix: "audit"
 
     encrypts :notes
   end
